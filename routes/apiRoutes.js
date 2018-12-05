92b417ccf7c46f5718c977510d6f47432188cd7a
gaoqi7 <gaoqi7@gmail.com>
2018-12-04 20:26:01 -0800

    finish the OAuth of Spotify part

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,3 @@
-var db = require("../models");
-
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
@@ -9,15 +7,15 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.post("/api/token", function(req, res) {
+    console.log(req.body);
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
